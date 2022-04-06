228c880358f28def7d346a0b9642e7af4fd79777
Vincent den Boer <vincent@youapt.eu>
2022-04-06 12:00:33 +0200

    - Implement session support in front-end - Fix situation where front-end and back-end restart at the same time and front-end cannot connect to backend when restarting

--- src/backend.ts
+++ src/backend.ts
@@ -3,14 +3,35 @@ export type BackendRequester = (url: string, init?: RequestInit) => Promise<Resp
 const BACKEND_ORIGIN = process.env.NODE_ENV === "development" ? "http://localhost:3031" : "https://backend.example.com";
 
 export async function createBackendConnection(): Promise<BackendRequester> {
-  const appCreation = await fetch(BACKEND_ORIGIN + "/app/create", {
-    method: "POST",
-  });
-  const { appId } = await appCreation.json();
-  return (url, init?) => {
+  const appId = await createAppId();
+  let sessionId: string | undefined;
+  return async (url, init?) => {
     init = init ?? {};
     init.headers = init.headers ?? {};
     (init.headers as any)["X-Application-ID"] = appId;
-    return fetch(BACKEND_ORIGIN + url, init);
+    if (sessionId) {
+      (init.headers as any)["X-Session-Id"] = sessionId;
+    }
+    const response = await fetch(BACKEND_ORIGIN + url, init);
+    const newSessionId = response.headers.get("X-Session-Id");
+    if (newSessionId) {
+      sessionId = newSessionId;
+    }
+    return response;
   };
+}
+
+async function createAppId() {
+  for (let attempt = 0; attempt < 5; ++attempt) {
+    try {
+      const appCreation = await fetch(BACKEND_ORIGIN + "/app/create", {
+        method: "POST",
+      });
+      const { appId } = await appCreation.json();
+      return appId;
+    } catch (err) {
+      await new Promise((resolve) => setTimeout(resolve, 500));
+    }
+  }
+  throw new Error(`Could not create backend app after 5 attempts`);
 }
