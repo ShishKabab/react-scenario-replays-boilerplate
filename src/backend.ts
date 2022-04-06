67cec3a700a0fdaa44c3b2eb5a87b35e747a2c2d
Vincent den Boer <vincent@youapt.eu>
2022-04-06 10:49:14 +0200

    Correctly send requests to backend

--- src/backend.ts
+++ src/backend.ts
@@ -1,12 +1,16 @@
 export type BackendRequester = (url: string, init?: RequestInit) => Promise<Response>;
 
+const BACKEND_ORIGIN = process.env.NODE_ENV === "development" ? "http://localhost:3031" : "https://backend.example.com";
+
 export async function createBackendConnection(): Promise<BackendRequester> {
-  const appCreation = await fetch("/app/create");
+  const appCreation = await fetch(BACKEND_ORIGIN + "/app/create", {
+    method: "POST",
+  });
   const { appId } = await appCreation.json();
   return (url, init?) => {
     init = init ?? {};
     init.headers = init.headers ?? {};
     (init.headers as any)["X-Application-ID"] = appId;
-    return fetch(url, init);
+    return fetch(BACKEND_ORIGIN + url, init);
   };
 }
