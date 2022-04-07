9d0d6ebcc5475ceaf478a36cdc7086e2d8534cc7
Vincent den Boer <vincent@youapt.eu>
2022-04-07 17:51:50 +0200

    Remove test to verify whether components are correctly unregistered

--- src/features/scenario-replays/decorators.tsx
+++ src/features/scenario-replays/decorators.tsx
@@ -2,10 +2,6 @@ import React, { ComponentType } from "react";
 import ScenarioReplayContext, { ScenarioReplayContextData } from "./react-context";
 import ComponentRegistry from "./registry";
 
-interface State {
-  test: boolean;
-}
-
 export function scenarioCallable() {
   return (target: Object, key: string | symbol, descriptor: PropertyDescriptor) => {
     const origFunction: (...args: any) => void = descriptor.value;
@@ -17,17 +13,9 @@ export function scenarioCallable() {
 
 export function enableScenarios() {
   return <Props extends object>(WrappedComponent: ComponentType<Props>) => {
-    return class ScenarioComponent<Props> extends React.Component<Props, State> {
+    return class ScenarioComponent<Props> extends React.Component<Props> {
       static contextType = ScenarioReplayContext;
       unregisterComponent?(): void;
-
-      constructor(props: Props) {
-        super(props);
-
-        this.state = {
-          test: true,
-        };
-      }
 
       handleRef = (ref: React.Component & { _componentRegistry?: ComponentRegistry }) => {
         const { componentRegistry } = this.context as ScenarioReplayContextData;
@@ -41,12 +29,7 @@ export function enableScenarios() {
       };
 
       render() {
-        return (
-          <div>
-            <div onClick={() => this.setState({ test: false })}>click me!</div>
-            {this.state.test && <WrappedComponent ref={this.handleRef} {...(this.props as any)} />}
-          </div>
-        );
+        return <WrappedComponent ref={this.handleRef} {...(this.props as any)} />;
       }
     };
   };
